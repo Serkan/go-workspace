a78a2069e9af6baf1b043d68f3fc2946d44f9ef9
Serkan Turgut <serkan.turgut0@gmail.com>
2016-08-23 21:25:08 +0300

    comment change and cs code experiments

--- file-operations-example/src/futils/fio.go
+++ file-operations-example/src/futils/fio.go
@@ -4,14 +4,15 @@ import (
 	"os"
 )
 
-func ToString(fileName string) (content string, err error){
+// ToString Dumps given file content to a string and returns it
+func ToString(fileName string) (content string, err error) {
 	f, e := os.Open(fileName)
 	defer f.Close()
 	if e != nil {
 		return "", err
 	}
-	buffer := make([]byte,1024)
-	for read, err := f.Read(buffer); read != 0 && err == nil; read, err = f.Read(buffer){
+	buffer := make([]byte, 1024)
+	for read, err := f.Read(buffer); read != 0 && err == nil; read, err = f.Read(buffer) {
 		content += content + string(buffer)
 	}
 	return content, err
